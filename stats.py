6b1f8543cac14801038dc64a19cc587f119cff37
Bhavesh Kalisetti <bhaveshk658@berkeley.edu>
2020-08-20 01:56:57 -0700

    Suppressing output

--- stats.py
+++ stats.py
@@ -1,7 +1,16 @@
 import cassiopeia as cass
+import os
 
 RIOT_API_KEY = "RGAPI-091c05d6-a248-47e0-b9fc-20e3913648b7"
 cass.set_riot_api_key(RIOT_API_KEY)
+
+# open 2 fds
+null_fds = [os.open(os.devnull, os.O_RDWR) for x in range(2)]
+# save the current file descriptors to a tuple
+save = os.dup(1), os.dup(2)
+# put /dev/null fds on 1 and 2
+os.dup2(null_fds[0], 1)
+os.dup2(null_fds[1], 2)
 
 me = cass.Summoner(name="PistoisorBlades", region="NA")
 
@@ -21,10 +30,13 @@ def check(match, matchup):
 def get_latest(matches, matchup):
     for match in matches:
         if check(match, matchup):
+            os.dup2(save[0], 1)
+            os.dup2(save[1], 2)
+            # close the temporary fds
+            os.close(null_fds[0])
+            os.close(null_fds[1])
             print("Match found")
             return match
 
     print("No recent matches found")
     return None
-
-get_latest(matches, "Garen")
